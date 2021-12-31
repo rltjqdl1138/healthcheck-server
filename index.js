14e8b93c8297d4a843cede8f80d8d206de778529
Kim ki seop <rltjqdl1138@naver.com>
2021-12-31 11:10:35 +0900

    update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cron = require('node-cron')
 const app = express()
 
 app.use(express.json())
@@ -60,80 +61,30 @@ app.post("/",(req, res)=>{
 })
 
 app.listen( 9001, ()=>console.log('Server is running on 9001'))
+cron.schedule("*/10 * * * * *", ()=>decreaseTTL())
+const decreaseTTL = ()=>{
+    Object.keys(map).forEach(e=>{
+        if(map[e].ttl < 0) delete map[e]
+        else map[e].ttl = map[e].ttl - 1
+    })
+}
+
 
 
 const map = {
     "123.123.123.123:8000":{
         info:"gameserver",
-        tags:["game"]
+        tags:["game"],
+        ttl:10
     },
     "123.123.123.123:8001":{
         info:"gameserver",
-        tags:["game"]
+        tags:["game"],
+        ttl:10
     },
     "123.123.123.123:8002":{
         info:"gameserver",
-        tags:["game"]
+        tags:["game"],
+        ttl:10
     },
 }
-
-
-
-class distributor extends require('./tcpServer.js'){
-    constructor(port=9000){
-        super("distributor", port, ["POST/distributes", "GET/distributes"])
-    }
-
-    onCreate(socket) {
-        //console.log("onCreate", socket.remoteAddress, socket.remotePort)
-        this.sendInfo(socket)
-    }
-
-    onClose(socket) {
-        var key = socket.remoteAddress + ":" + socket.remotePort
-        //console.log("onClose", socket.remoteAddress, socket.remotePort)
-        delete map[key]
-        this.sendInfo()
-    }
-
-    onRead(socket, json) {
-        var key = socket.remoteAddress + ":" + socket.remotePort
-        console.log("onRead", socket.remoteAddress, socket.remotePort, json)
-        //console.log("\n\n\n")
-        //console.log(json)
-        //console.log("\n\n\n")
-        if(json.uri == "/distributes" && json.method == "POST") {
-            map[key] = {
-                socket: socket
-            }
-            map[key].info = json.params
-            map[key].info.host = socket.remoteAddress
-            this.sendInfo()
-        }
-    }
-
-    write(socket, packet) {
-        socket.write(JSON.stringify(packet) + "¶")
-    }
-
-    sendInfo(socket) {
-        var packet = {
-            uri: "/distributes",
-            method: "GET",
-            key: 0,
-            params: []
-        }
-        for (var n in map){
-            packet.params.push(map[n].info)
-        }
-        if(socket){
-            this.write(socket, packet)
-        } else {
-            for (var n in map){
-                this.write(map[n].socket, packet)
-            }
-        }
-    }
-}
-
-new distributor()
