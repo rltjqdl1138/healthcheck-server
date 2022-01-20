bdeb47d6e518895a979dd20a97ee4c27364afe0d
Kim ki seop <rltjqdl1138@naver.com>
2022-01-20 17:19:20 +0900

    Update tempKakao.js

--- tempKakao.js
+++ tempKakao.js
@@ -3,7 +3,8 @@ const fetch = require('node-fetch')
 const app = express()
 const PORT = 8000
 const KAKAO_REST_KEY = "21b2924ede1c48a36cec40f7b08d9a6b"
-
+const MAP = {
+}
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
@@ -18,6 +19,7 @@ app.get("/test",async (req, res)=>{
     
     const bodyFromKakao = await getAccessTokenFromKakao(code)
     const data = await getDataFromKakao(bodyFromKakao.access_token)
+    MAP[data.id] = bodyFromKakao.access_token
 
     data.access_token = bodyFromKakao.access_token
    
@@ -26,6 +28,26 @@ app.get("/test",async (req, res)=>{
 app.post("/test",(req,res)=>{
     res.setHeader('Content-Type', 'application/json');
     res.end(req.body)
+})
+app.post("/auth/kakao/logout",async (req, res)=>{
+    res.setHeader('Content-Type', 'application/json');
+    const {id} = req.body
+    const accessToken = MAP[id]
+    
+    if(!accessToken)
+        return res.status(401).end()
+    delete MAP[id]
+
+    const response = await fetch("https://kapi.kakao.com/v1/user/logout",
+    {
+        method:'post',
+        headers:{
+            'Content-type':"application/x-www-form-urlencoded",
+            'Authorization': `Bearer ${accessToken}`
+        }
+    })
+    const data = await response.json()
+    res.json(data)
 })
 
 const makeHTMLForUnreal = (data)=>{
